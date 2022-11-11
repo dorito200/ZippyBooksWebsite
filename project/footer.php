bd52cea056904b387ba28b64f812463679b16d3e
dorito200 <jdarida9@gmail.com>
2022-11-11 13:31:40 -0600

    added the website

---
+++ project/footer.php
@@ -0,0 +1,39 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">cart</a>
+         <a href="orders.php">orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +123-456-7890 </p>
+         <p> <i class="fas fa-phone"></i> +111-222-3333 </p>
+         <p> <i class="fas fa-envelope"></i> jld224@uakron.edu </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Akron, Ohio. 44311 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/UniversityofAkron/"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="https://www.instagram.com/jake.darida/"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="https://app.joinhandshake.com/stu/users/25359764"> <i class="fa-regular fa-handshake"></i></i> handshake </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright <?php echo date('Y'); ?> by <span>Jake Darida</span> </p>
+
+</section>
